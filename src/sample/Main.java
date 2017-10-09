4d576da2e3e364163b36d105d33c8c501c0c597e
Jay Vince Serato <jdserato@up.edu.ph>
2017-10-09 16:26:45 +0800

    Added the buttons with limited functionality.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,12 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
-
-    Button btnBusSchedule;
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -23,14 +18,5 @@ public class Main extends Application implements EventHandler<ActionEvent> {
         primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-        btnBusSchedule.setOnAction(this);
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        if (event.getSource() == btnBusSchedule) {
-            System.out.println("WPPPP");
-        }
     }
 }
