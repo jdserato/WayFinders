a8f21830d23a9669d9397eddca13053eb41c07ad
Jay Vince Serato <jdserato@up.edu.ph>
2017-10-11 16:32:03 +0800

    Added small graphics.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,15 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class Controller {
 
     @FXML
-    private Button btnBusSchedule, btnRoutesAndFares, btnBusTerminal;
+    private Button btnRoutesAndFares, btnBusTerminal;
     private boolean btnBusScheduleClicked, btnBusRoutesAndFaresclicked, btnBusTerminalClicked;
+    @FXML
+    private ImageView btnBusSchedule;
 
     @FXML
     private void handleBtnBusScheduleClick() {
